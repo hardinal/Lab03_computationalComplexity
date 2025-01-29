9185171090d5e22d8138747b4e355bb4b087ea8c
hardinal <hardinal@appstate.edu>
2025-01-29 16:03:15 -0500

    c1

---
+++ src/main/algorithms/BigOh.java
@@ -0,0 +1,55 @@
+package algorithms;
+import java.util.Random;
+
+public class BigOh
+{
+    private final double MILLISECONDS_PER_SECOND;
+    private final int NUM_TRIALS;
+    private Random rand;
+
+    public BigOh()
+    {
+
+    }
+
+    public BigOh(Random rand)
+    {
+
+    }
+
+    public int runAlgorithm(int choice, int numElements)
+    {
+
+    }
+
+    public double bigOhFunc(int choice, double n)
+    {
+
+    }
+
+    public double timeAlgorithm(int choice, int n)
+    {
+
+    }
+
+    public double robustTimeAlgorithm(int choice, int n)
+    {
+
+    }
+
+    public double estimateTiming(int choice, int n1, double t1, int n2)
+    {
+
+    }
+
+    public double percentError(double correct, double estimate)
+    {
+
+    }
+
+    public double computePercentError(int choice, int n1, int n2)
+    {
+
+    }
+
+}
